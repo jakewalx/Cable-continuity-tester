f233a3eb5c266ab0f0632f406b433f27d7503f4e
jakewalx <jakewalx@gmail.com>
2024-05-11 00:03:23 +1000

    newest

--- code/code.ino
+++ code/code.ino
@@ -6,12 +6,15 @@
 
 #define RED_BRIGHTNESS 70
 
-#include <SoftPWM.h>        //by Brett Hagman
-#include <LiquidCrystal_I2C.h>
+#include <SoftPWM.h>
+#include <LiquidCrystal.h>
+#include <inttypes.h> // Add this line to include the "inttypes.h" header file
 
-//Declare global variables
+
+
+// Declare global variables
 int reset_happened = 0;
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 // available pins in this array
 int HW_P[] = {2, 3, 4, 5, 6, 7, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, A0, A1, A2, A3, A4, A5, A6, A7};
@@ -19,11 +22,11 @@ int HW_P[] = {2, 3, 4, 5, 6, 7, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 
 
 struct cable
 {
-  int tot_pins;     //Total number of pins
-  int CI = -1;      //Last sequential pin number of INPUT CONNECTOR
-  int CO_1 = -1;    //Last sequential pin number of OUTPUT CONNECTOR 1
-  int CO_2 = -1;    //Last sequential pin number of OUTPUT CONNECTOR 2
-  int CO_3 = -1;    //Last sequential pin number of OUTPUT CONNECTOR 3
+  int tot_pins;  // Total number of pins
+  int CI = -1;   // Last sequential pin number of INPUT CONNECTOR
+  int CO_1 = -1; // Last sequential pin number of OUTPUT CONNECTOR 1
+  int CO_2 = -1; // Last sequential pin number of OUTPUT CONNECTOR 2
+  int CO_3 = -1; // Last sequential pin number of OUTPUT CONNECTOR 3
 
   int T[50][5];
 };
@@ -31,51 +34,51 @@ struct cable
 struct err
 {
   int err_count = 0;
-  //int open_c = 0;
-  //int X = 0;
-  //int Y = 0;
-  //int short_c = 0;
+  // int open_c = 0;
+  // int X = 0;
+  // int Y = 0;
+  // int short_c = 0;
   int err_list[MAX_ERRORS][3];
 };
 
 err check_pin(cable C, const int i, const int SOE = 1)
 {
-  //TO DO
-  //Check if i belongs to the input connector
+  // TO DO
+  // Check if i belongs to the input connector
   err report;
   report.err_count = 0;
 
-  //Make sure MASTER_GOOD LED is NOT on when testing
+  // Make sure MASTER_GOOD LED is NOT on when testing
   digitalWrite(MASTER_GOOD, LOW);
 
   pinMode(HW_P[i], OUTPUT);
   digitalWrite(HW_P[i], HIGH);
-  delay(10);  //Wait for some capacitor to charge?
+  delay(10); // Wait for some capacitor to charge?
 
   int j = 0;
-  while(C.T[i][j] != -1)  //Check correct wires
+  while (C.T[i][j] != -1) // Check correct wires
   {
-    if(digitalRead(HW_P[C.T[i][j]]) == LOW)
+    if (digitalRead(HW_P[C.T[i][j]]) == LOW)
     {
-      if(report.err_count < MAX_ERRORS)
+      if (report.err_count < MAX_ERRORS)
       {
-        //digitalWrite(MASTER_ERROR, HIGH);
+        // digitalWrite(MASTER_ERROR, HIGH);
         SoftPWMSet(MASTER_ERROR, RED_BRIGHTNESS);
-        report.err_list[report.err_count][0] = i;          //ID of faulty pin
-        report.err_list[report.err_count][1] = 0;          //error ID (in this case open circuit)
-        report.err_list[report.err_count][2] = C.T[i][j];  //destination pin
-        report.err_count ++;
+        report.err_list[report.err_count][0] = i;         // ID of faulty pin
+        report.err_list[report.err_count][1] = 0;         // error ID (in this case open circuit)
+        report.err_list[report.err_count][2] = C.T[i][j]; // destination pin
+        report.err_count++;
       }
     }
     j++;
   }
-  
-  for(j = 0; j < C.tot_pins; j++)   //Check the rest of the wires
+
+  for (j = 0; j < C.tot_pins; j++) // Check the rest of the wires
   {
-    //Only check incorrect wires (correct ones have already been tested)
+    // Only check incorrect wires (correct ones have already been tested)
     int incorrect = 1;
     int k = 0;
-    while(C.T[i][k] != -1)  //go trough the current template line
+    while (C.T[i][k] != -1) // go trough the current template line
     {
       if (j == C.T[i][k])
       {
@@ -83,63 +86,63 @@ err check_pin(cable C, const int i, const int SOE = 1)
       }
       k++;
     }
-    if(j == i)
+    if (j == i)
     {
       incorrect = 0;
     }
 
-    if(incorrect == 1)   //Checking wires that are incorrect
+    if (incorrect == 1) // Checking wires that are incorrect
     {
-      //check for short-circuit errors
-      if(j <= C.CI && digitalRead(HW_P[j]) == HIGH && i < j)
+      // check for short-circuit errors
+      if (j <= C.CI && digitalRead(HW_P[j]) == HIGH && i < j)
       {
-        //digitalWrite(MASTER_ERROR, HIGH);
+        // digitalWrite(MASTER_ERROR, HIGH);
         SoftPWMSet(MASTER_ERROR, RED_BRIGHTNESS);
-        report.err_list[report.err_count][0] = i;          //ID of faulty pin
-        report.err_list[report.err_count][1] = 3;          //error ID (in this case short circuit)
-        report.err_list[report.err_count][2] = j;  //destination pin
-        report.err_count ++;
+        report.err_list[report.err_count][0] = i; // ID of faulty pin
+        report.err_list[report.err_count][1] = 3; // error ID (in this case short circuit)
+        report.err_list[report.err_count][2] = j; // destination pin
+        report.err_count++;
       }
-      //check for mismatch errors
-      if(j > C.CI && j <= C.CO_3 && digitalRead(HW_P[j]) == HIGH)
+      // check for mismatch errors
+      if (j > C.CI && j <= C.CO_3 && digitalRead(HW_P[j]) == HIGH)
       {
-        //digitalWrite(MASTER_ERROR, HIGH);
+        // digitalWrite(MASTER_ERROR, HIGH);
         SoftPWMSet(MASTER_ERROR, RED_BRIGHTNESS);
-        report.err_list[report.err_count][0] = i;          //ID of faulty pin
-        report.err_list[report.err_count][1] = 1;          //error ID (in this case mismatch)
-        report.err_list[report.err_count][2] = j;  //destination pin
-        report.err_count ++;
+        report.err_list[report.err_count][0] = i; // ID of faulty pin
+        report.err_list[report.err_count][1] = 1; // error ID (in this case mismatch)
+        report.err_list[report.err_count][2] = j; // destination pin
+        report.err_count++;
       }
     }
   }
-  //Visual LED debugging
+  // Visual LED debugging
   delay(1);
 
   print_S(report, i);
 
-  //Stop on error if required
-  if(SOE == 1 && report.err_count > 0)
+  // Stop on error if required
+  if (SOE == 1 && report.err_count > 0)
   {
-    delay(500); //Do not register previous long presses
-    while(1)
+    delay(500); // Do not register previous long presses
+    while (1)
     {
-      if(reset_happened == 0)
+      if (reset_happened == 0)
       {
         reset_happened = !digitalRead(BUTTON_RESET);
       }
       char next = Serial.read();
-      if(digitalRead(BUTTON_NEXT) == 0 || next == 'c' || next == 'C' || reset_happened)
+      if (digitalRead(BUTTON_NEXT) == 0 || next == 'c' || next == 'C' || reset_happened)
       {
-        while(Serial.read() >= 0)
+        while (Serial.read() >= 0)
         {
-          ; //Empty Serial buffer
+          ; // Empty Serial buffer
         }
-        break;  //exit infinite loop
+        break; // exit infinite loop
       }
     }
   }
 
-  //Put current pin back to INPUT and Return results
+  // Put current pin back to INPUT and Return results
   digitalWrite(HW_P[i], LOW);
   pinMode(HW_P[i], INPUT);
   delay(1);
@@ -154,7 +157,7 @@ void print_S(err report, int current_in_pin)
   Serial.print(": ");
   Serial.println(report.err_count);
 
-  while(i < report.err_count && i < MAX_ERRORS)
+  while (i < report.err_count && i < MAX_ERRORS)
   {
     Serial.print(report.err_list[i][0]);
     Serial.print(" ");
@@ -165,12 +168,12 @@ void print_S(err report, int current_in_pin)
   }
 }
 
-//Declare a cable variable and initialize it with data as described in the cable_template_format.txt file
+// Declare a cable variable and initialize it with data as described in the cable_template_format.txt file
 cable C;
 
 void setup()
 {
-  //Initializing the cable
+  // Initializing the cable
   /*
   C.tot_pins = 4;
   C.CI = 1;
@@ -186,13 +189,12 @@ void setup()
   //C.T[2][1] = -1;
   */
 
-  //complicated test cable
+  // complicated test cable
   C.CI = 14;
   C.CO_1 = 21;
   C.CO_2 = 28;
   C.CO_3 = 43;
   C.tot_pins = 44;
-
 
   C.T[0][0] = 17;
   C.T[0][1] = 29;
@@ -205,7 +207,7 @@ void setup()
   C.T[2][0] = 28;
   C.T[2][1] = 31;
   C.T[2][2] = -1;
-  
+
   C.T[3][0] = 15;
   C.T[3][1] = 32;
   C.T[3][2] = -1;
@@ -251,7 +253,6 @@ void setup()
   C.T[14][0] = 26;
   C.T[14][1] = 43;
   C.T[14][2] = -1;
-  
 
   SoftPWMBegin();
   SoftPWMSet(MASTER_ERROR, 0);
@@ -267,26 +268,25 @@ void setup()
 void loop()
 {
   reset_happened = 0;
-  
+
   Serial.println("##### BEGIN CABLE TESTING ... #####");
   int error_found = 0;
 
-  //Begin calling check algorithm
-  for(int in_pin = 0; in_pin <= C.CI; in_pin++)
+  // Begin calling check algorithm
+  for (int in_pin = 0; in_pin <= C.CI; in_pin++)
   {
     err rep = check_pin(C, in_pin);
-    if(rep.err_count > 0)
+    if (rep.err_count > 0)
     {
       error_found = 1;
     }
-    if(reset_happened)
+    if (reset_happened)
     {
       break;
     }
   }
 
-  
-  if(error_found == 0 && !reset_happened)
+  if (error_found == 0 && !reset_happened)
   {
     digitalWrite(MASTER_GOOD, HIGH);
     Serial.println("##### CABLE TESTING DONE -> CABLE OK (send r or R for rerun ... #####");
@@ -294,7 +294,7 @@ void loop()
   }
   else
   {
-    if(reset_happened)
+    if (reset_happened)
     {
       Serial.println("!!!!! RESET BUTTON PRESSED. RESETING...");
       Serial.println();
@@ -306,16 +306,16 @@ void loop()
     }
   }
 
-  while(true)
+  while (true)
   {
-    if(reset_happened == 0)
+    if (reset_happened == 0)
     {
       reset_happened = !digitalRead(BUTTON_RESET);
     }
     char Redo = Serial.read();
-    if(Redo == 'r' || Redo == 'R' || reset_happened)
+    if (Redo == 'r' || Redo == 'R' || reset_happened)
     {
-      while(Serial.read() >= 0)
+      while (Serial.read() >= 0)
       {
         ;
       }
@@ -324,6 +324,6 @@ void loop()
   }
 
   digitalWrite(MASTER_GOOD, LOW);
-  //digitalWrite(MASTER_ERROR, LOW);
+  // digitalWrite(MASTER_ERROR, LOW);
   SoftPWMSet(MASTER_ERROR, 0);
 }
